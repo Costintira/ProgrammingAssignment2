a9cc97ea6ad8712b4975da5f96511c2812bbb6f4
CostinTira <costin.tira@gmail.com>
2014-05-24 18:02:04 +0300

    finally made it!
    This should be the final version; tested it with
    b$set(matrix(c(4,3,3,2),2,2))

--- cachematrix.R
+++ cachematrix.R
@@ -4,8 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setcache.inverse <- function(solve) inv <<- solve
+        getcache.inverse <- function() inv
+        list(set = set, get = get,
+             setcache.inverse = setcache.inverse,
+             getcache.inverse = getcache.inverse)
         
-        print("The quick brown fox...")
+        
 
 }
 
@@ -14,4 +25,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getcache.inverse()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        inv <- solve(data, ...)
+        x$setcache.inverse(inv)
+        inv
 }
